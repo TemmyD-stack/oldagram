92f4dfed3060b269bd649d8d0be85fb0f2120377
TemmyD-stack <temi.dire.dayo.1@gmail.com>
2025-04-21 12:21:28 +0100

    fixed the display error
    the error is found in the caption

--- index.js
+++ index.js
@@ -8,6 +8,16 @@ const posts = [
         comment: "just took a few mushrooms lol",
         likes: 21
     },
+
+    {
+        name: "Joseph Ducreux",
+        username: "jd1735",
+        location: "Paris, France",
+        avatar: "assets/josh-avatar.png",
+        post: "assets/josee.png",
+        comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
+        likes: 152
+    },
     {
         name: "Gustave Courbet",
         username: "gus1819",
@@ -16,51 +26,54 @@ const posts = [
         post: "assets/gustavo.png",
         comment: "i'm feelin a bit stressed tbh",
         likes: 4
-    },
-    {
-        name: "Joseph Ducreux",
-        username: "jd1735",
-        location: "Paris, France",
-        avatar: "assets/josh-avatar.png",
-        post: "assets/Joseph.png",
-        comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
-        likes: 152
     }
 ]
 
 const postContainer = document.querySelector(".post-container");
+const maxWordsBeforeBreak = 40;
 
-for(let i = 0; i < posts.length; i++){
+for (let i = 0; i < posts.length; i++) {
     const poster = posts[i];
-    const postElement = 
-    `
-        <section class="section-one">
-            <div class="heading">
-                <img src=${poster.avatar} alt="profile picture" class="profile-picture">
-                <div class="main-heading">
-                    <h2>${poster.name}</h2>
-                    <p class="location">${poster.location}</p>
-                </div>
-            </div>
-            <img src=${poster.post} alt="Your picture post" class="post-picture" id="post-img">
-            <div class="reactions-top">
-                <div class="icon"><i class="fa-regular fa-heart" ></i></div>
-                <div class="icon"><i class="fa-regular fa-comment"></i></div>
-                <div class="icon"><i class="fa-regular fa-paper-plane"></i></div>
-            </div>
-            <div class="reaction-bottom">
-                <h3 class="no-of-likes" >${poster.likes} likes</h3>
-                <div class="caption">
-                    <p><span>${poster.username}</span>${poster.comment}</p>
-                </div>
-            </div>
+    const words = poster.comment.trim().split(/\s+/);
+    let formattedComment;
+  
+    if (words.length > maxWordsBeforeBreak) {
+      const firstLine = words.slice(0, maxWordsBeforeBreak).join(' ');
+      const rest = words.slice(maxWordsBeforeBreak).join(' ');
+      formattedComment = `${firstLine}<br>${rest}`;
+    } else {
+      formattedComment = poster.comment;
+    }
+  
+    const section = document.createElement('section');
+    section.classList.add('section-one');
+  
+    section.innerHTML = `
+      <div class="heading">
+        <img src="${poster.avatar}" alt="profile picture" class="profile-picture">
+        <div class="main-heading">
+          <h2>${poster.name}</h2>
+          <p class="location">${poster.location}</p>
+        </div>
+      </div>
+      <img src="${poster.post}" alt="Your picture post" class="post-picture" id="post-img">
+      <div class="reactions-top">
+        <div class="icon"><i class="fa-regular fa-heart"></i></div>
+        <div class="icon"><i class="fa-regular fa-comment"></i></div>
+        <div class="icon"><i class="fa-regular fa-paper-plane"></i></div>
+      </div>
+      <div class="reaction-bottom">
+        <h3 class="no-of-likes">${poster.likes} likes</h3>
+        <div class="caption">
+          <p><span>${poster.username}</span> ${formattedComment}</p>
+        </div>
+      </div>
+    `;
+  
+    postContainer.appendChild(section);
+  }
 
-        </section>
-    `
-    ;
-
-    postContainer.innerHTML += postElement;
-}
+  
 const likeBtn = document.querySelector("#like-btn");
 
 // likeBtn.addEventListener.forEach(btn => {
